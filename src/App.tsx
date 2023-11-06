a12a08a49ad8893ce114a4ee926f502d08383ffa
Gauthierboddaert <Boddaert.gauthier@gmail.com>
2023-11-06 13:45:11 +0100

    add react query

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,24 @@ import { BrowserRouter, Routes } from "react-router-dom";
 import Layout from "./features/header/components/Layout";
 import routes from "./pages/Routes";
 import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const App = () => {
+  const client = new QueryClient()
   return (
     <>
-      <BrowserRouter future={{ v7_startTransition: true }} basename="/">
-        <div className="flex flex-col md:flex-row">
-        <Layout/>
-        <Routes>
-          {routes.map((route: ReactNode) => {
-            return route;
-          })}
-        </Routes>
-        </div>
-      </BrowserRouter>
+      <QueryClientProvider client={client}>
+        <BrowserRouter future={{ v7_startTransition: true }} basename="/">
+          <div className="flex flex-col md:flex-row">
+            <Layout />
+            <Routes>
+              {routes.map((route: ReactNode) => {
+                return route;
+              })}
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </QueryClientProvider>
     </>
   );
 };
