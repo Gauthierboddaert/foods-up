a2166e1ed020e06c5b6cfc28187e358c3061b952
Gauthierboddaert <Boddaert.gauthier@gmail.com>
2023-12-18 14:01:03 +0100

    add animation

--- src/features/search/components/SearchCard.tsx
+++ src/features/search/components/SearchCard.tsx
@@ -1,13 +1,39 @@
+import { useState } from "react";
 import FilterIcon from "../../Icon/FilterIcon";
 import SearchBar from "./SearchBar";
+import { motion } from "framer-motion";
 
 const SearchCard = () => {
-  
+  const [displayFilter, setDisplayFilter] = useState(false);
+
+  const onClick = () => {
+    if (displayFilter) {
+      setDisplayFilter(false);
+    } else {
+      setDisplayFilter(true);
+    }
+  };
+
   return (
     <div className="w-full">
       <SearchBar />
-      <div className="">
+      <div onClick={() => onClick()} className="w-min">
         <FilterIcon className="w-[20px] h-[20px] fill-current" />
+      </div>
+
+      <div className={`${displayFilter ? "flex" : "hidden"} justify-center`}>
+        <motion.div
+          initial={{
+            opacity: 1,
+            scale: displayFilter ? 0.8 : 0,
+          }}
+          animate={{
+            opacity: 1,
+            scale: displayFilter ? 1 : 0,
+          }}
+          transition={{ type: "spring", stiffness: 120 ,duration: 0.1 }}
+          className={` w-3/4 h-[300px] bg-white absolute mt-12 rounded-lg border-[1px] border-gray-200 shadow-md z`}
+        ></motion.div>
       </div>
     </div>
   );
